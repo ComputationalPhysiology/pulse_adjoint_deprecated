83d2f4231a5ae216cd6a5714472a6deb5e7bfb41
Henrik Finsberg <henriknf@simula.no>
2018-01-31 20:09:31 +0100

    Add requirements to setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,46 @@
 # System imports
-from distutils.core import setup
-import platform
+import os
 import sys
-from os.path import join as pjoin
+import subprocess
+import string
+import platform
+
+from setuptools import setup, find_packages, Command
 
 # Version number
 major = 1
 minor = 0
 
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+
+
+if platform.system() == "Windows" or "bdist_wininst" in sys.argv:
+    # In the Windows command prompt we can't execute Python scripts
+    # without a .py extension. A solution is to create batch files
+    # that runs the different scripts.
+    batch_files = []
+    for script in scripts:
+        batch_file = script + ".bat"
+        f = open(batch_file, "w")
+        f.write('python "%%~dp0\%s" %%*\n' % os.path.split(script)[1])
+        f.close()
+        batch_files.append(batch_file)
+    scripts.extend(batch_files)
+
+
+if on_rtd:
+    REQUIREMENTS = []
+else:
+    REQUIREMENTS = [
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "mesh_generation>=0.1!"
+        "pyyaml",
+        "h5py"        
+    ]
+
+dependency_links = ["https://bitbucket.org/finsberg/mesh_generation/get/master.tar.gz#egg=mesh_generation-0.1"]
 
 setup(name = "pulse_adjoint",
       version = "{0}.{1}".format(major, minor),
@@ -16,6 +49,9 @@ setup(name = "pulse_adjoint",
       """,
       author = "Henrik Finsberg",
       author_email = "henriknf@simula.no",
+      license="LGPL version 3 or later",
+      install_requires=REQUIREMENTS,
+      dependency_links=dependency_links,
       packages = ["pulse_adjoint",
                   "pulse_adjoint.models",
                   "pulse_adjoint.postprocess",
